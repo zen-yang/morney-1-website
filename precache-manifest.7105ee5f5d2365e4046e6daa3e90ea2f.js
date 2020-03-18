033830ba2fd705fde0f64e206bde1826810e1a40
zen-yang <519796642@qq.com>
2020-03-18 23:42:58 +0800

    deploy

---
+++ precache-manifest.7105ee5f5d2365e4046e6daa3e90ea2f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d7dbaf7c925edbf12c8b",
+    "url": "/morney-1-website/css/app.320a973e.css"
+  },
+  {
+    "revision": "c9129d947600e242a3ad331d9f3442e1",
+    "url": "/morney-1-website/index.html"
+  },
+  {
+    "revision": "d7dbaf7c925edbf12c8b",
+    "url": "/morney-1-website/js/app.bbf041a4.js"
+  },
+  {
+    "revision": "15753e352f7805d5821d",
+    "url": "/morney-1-website/js/chunk-vendors.7bed88d0.js"
+  },
+  {
+    "revision": "6576646f5c9ff69f4a17734836f662b5",
+    "url": "/morney-1-website/manifest.json"
+  },
+  {
+    "revision": "0eaee759fdbef4facecab223158b28d4",
+    "url": "/morney-1-website/qrcode.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/morney-1-website/robots.txt"
+  }
+]);
